3f10956fa310aedb7dac27f183b6ce700549a244
Leonel Alanis <l.alanis1989@gmail.com>
2023-01-10 19:18:50 -0600

    Fixed LoginForm

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -17,7 +17,7 @@ function LoginForm() {
 
     async function handleSubmit(e) {
         e.preventDefault()
-        const response = await fetch(`http://localhost:5000/authentication`, {
+        const response = await fetch(`http://localhost:5000/authentication/`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -29,10 +29,11 @@ function LoginForm() {
 
         if (response.status === 200) {
             setCurrentUser(data.user)
-            history.push('/')
-        } else (
+            history.push(`/`)
+        } else {
             setErrorMessage(data.message)
-        )
+        }
+
     }
 
     return (
