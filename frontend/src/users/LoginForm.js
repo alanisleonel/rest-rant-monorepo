978dfb86caf8b40cebbe8fbc9e1bcc13be3d6676
Leonel Alanis <l.alanis1989@gmail.com>
2023-01-11 23:31:03 -0600

    Authentication with JWT

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -16,7 +16,6 @@ function LoginForm() {
     const [errorMessage, setErrorMessage] = useState(null)
 
     async function handleSubmit(e) {
-        e.preventDefault()
         const response = await fetch(`http://localhost:5000/authentication/`, {
             method: 'POST',
             headers: {
@@ -29,11 +28,11 @@ function LoginForm() {
 
         if (response.status === 200) {
             setCurrentUser(data.user)
+            localStorage.setItem('token', data.token)
             history.push(`/`)
         } else {
             setErrorMessage(data.message)
         }
-
     }
 
     return (
